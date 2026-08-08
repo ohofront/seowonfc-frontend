b6585f4fe1621d299162289454993bb1f08d8956
ohoCoding <youngho7482@gmail.com>
2026-08-08 21:45:54 +0900

    feat: add admin tools and application routing

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import {Route,Routes} from 'react-router-dom';
+import {Layout} from './components/Layout';
+import {NotFound} from './components/UI';
+import {ProtectedRoute} from './components/ProtectedRoute';
+import HomePage from './pages/HomePage';
+import {LoginPage,SignupPage} from './pages/AuthPages';
+import {NewsDetailPage,NewsListPage} from './pages/NewsPages';
+import {PlayerDetailPage,PlayersPage} from './pages/PlayerPages';
+import {MatchesPage,StandingsPage} from './pages/MatchPages';
+import {BoardListPage,PostDetailPage,PostWritePage} from './pages/CommunityPages';
+import {EventDetailPage,EventsPage,SponsorsPage} from './pages/ExtraPages';
+import MyPage from './pages/MyPage';
+import AdminPage from './pages/AdminPage';
+
+export default function App(){return <Routes><Route element={<Layout/>}><Route index element={<HomePage/>}/><Route path="login" element={<LoginPage/>}/><Route path="signup" element={<SignupPage/>}/><Route path="news" element={<NewsListPage/>}/><Route path="news/:id" element={<NewsDetailPage/>}/><Route path="players" element={<PlayersPage/>}/><Route path="players/:id" element={<PlayerDetailPage/>}/><Route path="matches" element={<MatchesPage/>}/><Route path="standings" element={<StandingsPage/>}/><Route path="boards/:boardType" element={<BoardListPage/>}/><Route path="boards/:boardType/:postId" element={<PostDetailPage/>}/><Route path="sponsors" element={<SponsorsPage/>}/><Route path="events" element={<EventsPage/>}/><Route path="events/:id" element={<EventDetailPage/>}/><Route element={<ProtectedRoute/>}><Route path="boards/:boardType/write" element={<PostWritePage/>}/><Route path="mypage" element={<MyPage/>}/></Route><Route element={<ProtectedRoute admin/>}><Route path="admin" element={<AdminPage/>}/></Route><Route path="*" element={<NotFound/>}/></Route></Routes>}
